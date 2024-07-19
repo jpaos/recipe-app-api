51f5d102fbcb69357ab754e27cf62956f73ffc03
João Oliveira <joao.p.oliveira@iscte.pt>
2024-07-19 10:20:47 +0100

    Configuration added to wait for database.

---
+++ app/core/tests/test_commands.py
@@ -0,0 +1,46 @@
+"""
+Test Django management commands.
+"""
+
+from unittest.mock import patch
+
+from psycopg2 import OperationalError as Psycopg2Error
+
+from django.core.management import call_command
+from django.db.utils import OperationalError
+from django.test import SimpleTestCase
+
+
+@patch('core.management.commands.wait_for_db.Command.check')
+class CommandTests(SimpleTestCase):
+    """Test commands."""
+
+    def test_wait_for_db_ready(self, patched_check):
+        """Test waiting for database if database ready."""
+        patched_check.return_value = True
+
+        call_command('wait_for_db')
+
+        patched_check.assert_called_once_with(databases=['default'])
+
+    @patch('time.sleep')
+    def test_wait_for_db_delay(self, patched_sleep, patched_check):
+        """Test waiting for database when getting OperationalError."""
+
+        """
+        This is the way we throw exceptions instead of returning a
+        simple value (True of False). Recall that patch is simulating the
+        behaviour of the connection to the database.
+        """
+        patched_check.side_effect = [Psycopg2Error] * 2 + \
+            [OperationalError] * 3 + [True]
+
+        call_command('wait_for_db')
+
+        """
+        We expect to call the method 6 times until we get a True.
+        That's defined in patched_check list.
+        """
+        self.assertEqual(patched_check.call_count, 6)
+
+        patched_check.assert_called_with(databases=['default'])
